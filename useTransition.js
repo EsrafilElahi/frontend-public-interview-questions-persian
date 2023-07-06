4d3be17b43124c795492f78aa9906d99b8d22d2f
Esrafil <essinho1121@gmail.com>
2023-07-06 20:04:55 +0330

    Update useTransition.js

--- useTransition.js
+++ useTransition.js
@@ -4,3 +4,25 @@
 
 ولی وقتی از ترنزیشن استفاده کنیم. اون بخشی که سنگین هست رو میاد اولویت پایین میده و با تاخیر آپدیت میکنه یو آی اون بخش رو 
 و پندینگ هم داره برای هندل کردن این تاخیر 
+
+// code example
+import { useTransition } from 'react';
+
+export default function TabButton({ children, isActive, onClick }) {
+  const [isPending, startTransition] = useTransition();
+  if (isActive) {
+    return <b>{children}</b>
+  }
+  if (isPending) {
+    return <b className="pending">{children}</b>;
+  }
+  return (
+    <button onClick={() => {
+      startTransition(() => {
+        onClick();
+      });
+    }}>
+      {children}
+    </button>
+  );
+}
